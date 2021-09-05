c8dbb3bae12bab23405b252f103621f8261b8572
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-05 13:53:27 +0530

    Update AlexaList.js

--- plugins/AlexaList.js
+++ plugins/AlexaList.js
@@ -76,7 +76,7 @@ if (Config.WORKTYPE == 'private') {
             );
             if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
             await message.client.sendMessage(
-                message.jid,'*●    WhatsAlexa    ●*\n❉───────────❉\n\n' + CMD_HELP, MessageType.text,{quoted: message.data, contextInfo: { forwardingScore: 49, isForwarded: true }}
+                message.jid,'*●    Whatsdevil    ●*\n❉───────────❉\n\n' + CMD_HELP, MessageType.text,{quoted: message.data, contextInfo: { forwardingScore: 49, isForwarded: true }}
             );
         }
     }));
