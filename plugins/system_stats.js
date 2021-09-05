7fe2374ea104203a0665166eafa53c6cc1f250b7
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-05 10:10:17 +0530

    Create system_stats.js

---
+++ plugins/system_stats.js
@@ -0,0 +1,53 @@
+let WhatsAlexa = require('../events');
+let {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
+let {spawnSync} = require('child_process');
+let Config = require('../config');
+let chalk = require('chalk');
+let axios = require('axios');
+let Language = require('../language');
+let Lang = Language.getString('system_stats');
+
+if (Config.WORKTYPE == 'private') {
+
+    WhatsAlexa.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+
+       let pp
+        try { pp = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); } catch { pp = await message.client.getProfilePicture(); }
+        await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => { await message.client.sendMessage(message.jid, res.data, MessageType.image, {mimetype: Mimetype.png, quoted: message.data, contextInfo: { forwardingScore: 2, isForwarded: true }, caption: '```💕 Hey Bro!! I am still alive & kicking 😙```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n\n*Developer:* BuDy X\n\n*Git :* https://github.com/MrChaby/Whatshttps://github.com/TOXIC-DEVIL/WhatsAlexa.gitAlexa\n\n```💕 Copyright © 2021 💞\n Also Feel free to contribute & issue ( report issues & feature request on issue session of the ropo ).. ( https://github.com/TOXIC-DEVIL/WhatsAlexa ) 🙂❤️```' }); });
+    }));
+
+    WhatsAlexa.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    WhatsAlexa.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+
+        let pp
+        try { pp = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); } catch { pp = await message.client.getProfilePicture(); }
+        await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => { await message.client.sendMessage(message.jid, res.data, MessageType.image, {mimetype: Mimetype.png, quoted: message.data, contextInfo: { forwardingScore: 2, isForwarded: true }, caption: '```💕 Hey Bro!! I am still alive & kicking 😙```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n\n*Developer:* BuDy X\n\n*Git :* https://github.com/TOXIC-DEVIL/WhatsAlexa.git\n\n```💕 Copyright © 2021 💞\n Also Feel free to contribute & issue ( report issues & feature request on issue session of the ropo ).. ( https://github.com/TOXIC-DEVIL/WhatsAlexa ) 🙂❤️```' }); });
+    }));
+
+    WhatsAlexa.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+}
