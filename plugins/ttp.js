4e7834bc5e968ec5305e510b598bc6e2119c18b6
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-06 17:30:25 +0530

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,108 +1,85 @@
+let simpleGit = require('simple-git');
+let git = simpleGit();
 let WhatsAlexa = require('../events');
-let { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-let fs = require('fs');
-let axios = require('axios');
+let {MessageType} = require('@adiwajshing/baileys');
 let Config = require('../config');
+let exec = require('child_process').exec;
+let Heroku = require('heroku-client');
+let { PassThrough } = require('stream');
+let heroku = new Heroku({ token: Config.HEROKU.API_KEY })
 let Language = require('../language');
-let Lang = Language.getString('ttp');
+let Lang = Language.getString('updater');
 
-if (Config.WORKTYPE == 'private') {
+WhatsAlexa.addCommand({pattern: 'update$', fromMe: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+    if (commits.total === 0) {
+        await message.client.sendMessage(
+            message.jid,
+            Lang.UPDATE, MessageType.text
+        );    
+    } else {
+        var degisiklikler = Lang.NEW_UPDATE;
+        commits['all'].map(
+            (commit) => {
+                degisiklikler += '🆕🎉 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n\n*WhatsAlexa*';
+            }
+        );
 
-    WhatsAlexa.addCommand({ pattern: 'ttp ?(.*)', fromMe: true, desc: Lang.TTP_DESC }, (async (message, match) => {
+        await message.client.sendMessage(
+            message.jid,
+            degisiklikler + '```', MessageType.text
+        ); 
+    }
+}));
 
-        if (message.jid === '905524317852-1612300121@g.us') {
+WhatsAlexa.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_DESC, dontAddCommandList: true}, (async (message, match) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+    if (commits.total === 0) {
+        return await message.client.sendMessage(
+            message.jid,
+            Lang.UPDATE, MessageType.text
+        );    
+    } else {
+        var guncelleme = await message.reply(Lang.UPDATING);
+        if (Config.HEROKU.HEROKU) {
+            try {
+                var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
+            } catch {
+                return await message.client.sendMessage(
+                    message.jid,Lang.INVALID_HEROKU, MessageType.text);
+            }
 
-            return;
+            git.fetch('upstream', Config.BRANCH);
+            git.reset('hard', ['FETCH_HEAD']);
+
+            var git_url = app.git_url.replace(
+                "https://", "https://api:" + Config.HEROKU.API_KEY + "@"
+            )
+
+            try {
+                await git.addRemote('heroku', git_url);
+            } catch { console.log('heroku remote ekli'); }
+            await git.push('heroku', Config.BRANCH);
+
+            await message.client.sendMessage(
+                message.jid,Lang.UPDATED, MessageType.text);
+
+            await message.sendMessage(Lang.AFTER_UPDATE);
+
+        } else {
+            git.pull((async (err, update) => {
+                if(update && update.summary.changes) {
+                    await message.client.sendMessage(
+                        message.jid,Lang.UPDATED_LOCAL, MessageType.text);
+                    exec('npm install').stderr.pipe(process.stderr);
+                } else if (err) {
+                    await message.client.sendMessage(
+                        message.jid,'*❌ An Error Occurred!*\n*Error:* ```' + err + '```', MessageType.text);
+                }
+            }));
+            await guncelleme.delete();
         }
-
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
-        
-        var uri = encodeURI(match[1]);
-
-        var ttinullimage = await axios.get('https://api.xteam.xyz/ttp?file&text=' + uri, { responseType: 'arraybuffer' })
-
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CK, quoted: message.data})
-
-    }));
-
-    WhatsAlexa.addCommand({ pattern: 'attp ?(.*)', fromMe: true, desc: Lang.ATTP_DESC }, (async (message, match) => {
-
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
-        
-        var uri = encodeURI(match[1]);
-
-        var ttinullimage = await axios.get('https://api.xteam.xyz/attp?file&text=' + uri, { responseType: 'arraybuffer' })
-
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp, quoted: message.data})
-
-    }));
-    
-    WhatsAlexa.addCommand({pattern: 'emoji ?(.*)', fromMe: true, desc: Lang.EMOJI_DESC}, (async (message, match) => {
-
-      if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
-        
-      var uri = encodeURI(match[1]);
-  
-      var ttinullimage = await axios.get('https://api.zeks.xyz/api/emoji-image?apikey=odsMYXx67ZhT38w5hp5mgRKO8En&emoji='+ uri, { responseType: 'arraybuffer' })
-  
-      await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CK, quoted: message.data})
-  
-    }));
-}
-else if (Config.WORKTYPE == 'public') {
-
-    WhatsAlexa.addCommand({ pattern: 'ttp ?(.*)', fromMe: false, desc: Lang.TTP_DESC }, (async (message, match) => {
-
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
-        
-        var uri = encodeURI(match[1]);
-
-        var ttinullimage = await axios.get('https://api.xteam.xyz/ttp?file&text=' + uri, { responseType: 'arraybuffer' })
-
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CK, quoted: message.data})
-
-    }));
-
-    WhatsAlexa.addCommand({ pattern: 'attp ?(.*)', fromMe: false, desc: Lang.ATTP_DESC }, (async (message, match) => {
-
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
-        
-        var uri = encodeURI(match[1]);
-
-        var ttinullimage = await axios.get('https://api.xteam.xyz/attp?file&text=' + uri, { responseType: 'arraybuffer' })
-
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp, quoted: message.data})
-
-    }));
-    
-    WhatsAlexa.addCommand({pattern: 'emoji ?(.*)', fromMe: false, desc: Lang.EMOJI_DESC}, (async (message, match) => {
-
-      if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
-        
-      var uri = encodeURI(match[1]);
-  
-      var ttinullimage = await axios.get('https://api.zeks.xyz/api/emoji-image?apikey=odsMYXx67ZhT38w5hp5mgRKO8En&emoji='+ uri, { responseType: 'arraybuffer' })
-  
-      await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CK, quoted: message.data})
-  
-    }));
-}
+    }
+}));
