e2341b5f95263ae0cc990bad0cc52831ec54a015
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-08 12:33:36 +0530

    Update mentionall.js

--- plugins/mentionall.js
+++ plugins/mentionall.js
@@ -1,18 +1,8 @@
 let WhatsAlexa = require('../events');
 let {MessageType} = require('@adiwajshing/baileys');
 let Language = require('../language');
-let Config = require('../config');
-let TLang = Language.getString('tagall');
-let Lang = Language.getString('admin');
-let td = Config.WORKTYPE == 'public' ? false : true
+let Lang = Language.getString('tagall');
 
- async function checkUsAdmin(message, user = message.data.participant) {
-    var grup = await message.client.groupMetadata(message.jid);
-    var sonuc = grup['participants'].map((member) => {     
-        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
-    });
-    return sonuc.includes(true);
-}
 async function checkImAdmin(message, user = message.client.user.jid) {
     var grup = await message.client.groupMetadata(message.jid);
     var sonuc = grup['participants'].map((member) => {
@@ -21,30 +11,11 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-WhatsAlexa.addCommand({pattern: 'mentionall', fromMe: td, desc: TLang.TAGALL_DESC}, (async (message, match) => {
+WhatsAlexa.addCommand({pattern: 'mentionall', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
 
-    var im = await checkImAdmin(message);
-    var us = await checkUsAdmin(message);
-    if (!us) return await message.client.sendMessage(message.jid,Lang.USER_NOT_ADMIN ,MessageType.text);
-    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
+   var im = await checkImAdmin(message);
+   if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
 
-    grup = await message.client.groupMetadata(message.jid);
-    var jids = [];
-    mesaj = '';
-    grup['participants'].map(
-        async (uye) => {
-            mesaj += '▫@' + uye.id.split('@')[0] + ' ';
-            jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-        }
-    );
-    await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-}));
-
-WhatsAlexa.addCommand({pattern: 'mentionall', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-
-    var im = await checkImAdmin(message);
-    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
-    
     grup = await message.client.groupMetadata(message.jid);
     var jids = [];
     mesaj = '';
