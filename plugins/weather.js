27a736e87b2d55b8d88cc46fce3c76b4366f8d47
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-12 18:53:11 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -1,18 +1,23 @@
-let WhatsAlexa = require('../events');
-let {MessageType} = require('@adiwajshing/baileys');
-let got = require('got');
-let Config = require('../config');
-let Language = require('../language');
-let Lang = Language.getString('weather');
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
 
 if (Config.WORKTYPE == 'private') {
 
-    WhatsAlexa.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
-
-            if (message.jid === '905524317852-1612300121@g.us') {
-
-                return;
-            }
+    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
 	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
@@ -32,12 +37,24 @@ if (Config.WORKTYPE == 'private') {
 }
 if (Config.WORKTYPE == 'public') {
 
-    WhatsAlexa.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
+    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
 
-            if (message.jid === '905524317852-1612300121@g.us') {
-
-                return;
-            }
+	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
+	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
+	    try {
+		    const response = await got(url);
+		    const json = JSON.parse(response.body);
+		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n\n' +
+		    '*☀ ' + Lang.TEMP +':* ```' + json.main.temp_max + '°```\n' + 
+		    '*ℹ ' + Lang.DESC +':* ```' + json.weather[0].description + '```\n' +
+		    '*☀ ' + Lang.HUMI +':* ```%' + json.main.humidity + '```\n' + 
+		    '*💨 ' + Lang.WIND +':* ```' + json.wind.speed + 'm/s```\n' + 
+		    '*☁ ' + Lang.CLOUD +':* ```%' + json.clouds.all + '```\n', MessageType.text);
+	    } catch {
+		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
+	    }
+    });
+    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true, dontAddCommandList: true}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
 	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
