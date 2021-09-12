c83f51bc5e3c2d4eb9067e5d2796fddff93a99b9
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-12 19:35:36 +0530

    Update scan.js

--- plugins/scan.js
+++ plugins/scan.js
@@ -1,36 +1,40 @@
-let WhatsAlexa = require('../events');
-let {MessageType} = require('@adiwajshing/baileys');
-let Config = require('../config');
-let Language = require('../language');
-let Lang = Language.getString('tagall');
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('tagall');
 
 if (Config.WORKTYPE == 'private') {
-  
-    WhatsAlexa.addCommand({ pattern: 'scan ?(.*)', fromMe: true, desc: Lang.SCAN}, (async (message, match) => { 
+    Asena.addCommand({ pattern: 'scan ?(.*)', fromMe: true, desc: Lang.SCAN}, (async (message, match) => { 
 
-        if (match[1] == '') return await message.client.sendMessage(message.jid, Lang.NO, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+        if (match[1] == '') return await message.client.sendMessage(message.jid, Lang.NO, MessageType.text);
 
         var exists = await message.client.isOnWhatsApp(match[1])
         if (exists) {
-            await message.client.sendMessage(message.jid, '```' + match[1] + '``` \n' + Lang.SUC + '\n' + exists.jid, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+            await message.client.sendMessage(message.jid, '```' + match[1] + '``` \n' + Lang.SUC + '\n' + exists.jid, MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,'```' + match[1] + '``` \n' + Lang.UNSUC, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+            await message.client.sendMessage(message.jid,'```' + match[1] + '``` \n' + Lang.UNSUC, MessageType.text);
         }
     }));
 }
 else if (Config.WORKTYPE == 'public') {
-  
-    WhatsAlexa.addCommand({ pattern: 'scan ?(.*)', fromMe: false, desc: Lang.SCAN}, (async (message, match) => { 
+    Asena.addCommand({ pattern: 'scan ?(.*)', fromMe: false, desc: Lang.SCAN}, (async (message, match) => { 
 
-        if (match[1] == '') return await message.client.sendMessage(message.jid, Lang.NO, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+        if (match[1] == '') return await message.client.sendMessage(message.jid, Lang.NO, MessageType.text);
 
         var exists = await message.client.isOnWhatsApp(match[1])
         if (exists) {
-            await message.client.sendMessage(message.jid, '```' + match[1] + '``` \n' + Lang.SUC + '\n' + exists.jid, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+            await message.client.sendMessage(message.jid, '```' + match[1] + '``` \n' + Lang.SUC + '\n' + exists.jid, MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,'```' + match[1] + '``` \n' + Lang.UNSUC, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+            await message.client.sendMessage(message.jid,'```' + match[1] + '``` \n' + Lang.UNSUC, MessageType.text);
         }
     }));
 }
